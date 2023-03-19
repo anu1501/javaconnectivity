abb8d6ba1931aa908986805c2b28b37d73e7fb5b
Anu <anu392154@gmail.com>
2023-03-19 12:26:47 +0530

    creating java connection

---
+++ AddInteger.java
@@ -0,0 +1,15 @@
+class AddInteger{
+
+    public static void main(String[] args){
+        
+        System.out.println("Entwe two numbers");
+        int first = 10;
+        int second = 20;
+
+        System.out.println(first + " " +second);
+
+        //add two numbers
+        int sum = first + second;
+        System.out.println("The sum is: "+sum);
+    }
+}
